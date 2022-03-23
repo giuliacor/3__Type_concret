b50356fc730fa76beb64afea2749d1aa3c20daef
giuliacor <giulia02.cor@gmail.com>
2022-03-23 01:14:52 +0100

    cannot use the function vector.clear(position)

--- test.cc
+++ test.cc
@@ -16,15 +16,21 @@ int main(){
     bool loop(false);
     char N;
     do{
-        construct();
+        epfl.push_back(construct());
         cout << "Un autre etudiant ? (s/n) ";
         cin >> N;
         if (N == 's') loop = true;
         else loop = false;
     }while (loop);
     //verification doublons
-    
+    for (int i=0; i<epfl.size()-1; ++i) {
+        for (int j=i+1; j<epfl.size(); ++j) {
+            if (compare(epfl[i], epfl[j])) cout << "no\n";
+        }
+    }
     //affichage
+    for(auto etu : epfl)
+        affiche(etu);
     return 0;
 }
 
